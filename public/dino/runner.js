0b80a3ce28411218a8bec749ea92ce46d958848f
uphiago <hollyvolks@hotmail.com>
2026-07-31 17:27:27 -0300

    fix: remove intro-width animation that caused startup flicker

--- public/dino/runner.js
+++ public/dino/runner.js
@@ -476,32 +476,8 @@
             if (!this.activated && !this.crashed) {
                 this.playingIntro = true;
                 this.tRex.playingIntro = true;
-
-                // CSS animation definition.
-                var keyframes = '@-webkit-keyframes intro { ' +
-                    'from { width:' + Trex.config.WIDTH + 'px }' +
-                    'to { width: ' + this.dimensions.WIDTH + 'px }' +
-                    '}';
-                
-                // create a style sheet to put the keyframe rule in 
-                // and then place the style sheet in the html head    
-                var sheet = document.createElement('style');
-                sheet.innerHTML = keyframes;
-                document.head.appendChild(sheet);
-
-                this.containerEl.addEventListener(Runner.events.ANIM_END,
-                    this.startGame.bind(this));
-
-                this.containerEl.style.webkitAnimation = 'intro .4s ease-out 1 both';
-                this.containerEl.style.width = this.dimensions.WIDTH + 'px';
-
-                // if (this.touchController) {
-                //     this.outerContainerEl.appendChild(this.touchController);
-                // }
-                this.playing = true;
-                this.activated = true;
-            } else if (this.crashed) {
-                this.restart();
+                // Embedded: skip the intro-width animation (causes flicker).
+                this.startGame();
             }
         },
 
